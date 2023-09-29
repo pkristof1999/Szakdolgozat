62b31cb399bd52a52291f073e0077c7ea7756905
Pankotai Kristóf Géza <p.kristof1999@gmail.com>
2023-09-29 17:18:45 +0200

    Működik a profilkép visszaállítása gomb, egyelőre csak egy tesztképpel.

--- src/main/python/registerAccount.py
+++ src/main/python/registerAccount.py
@@ -1,7 +1,6 @@
-from PyQt6 import QtCore
-
 import welcomeScreen
 
+from PyQt6 import QtCore
 from PyQt6.uic import loadUi
 from PyQt6.QtWidgets import QMainWindow, QPushButton, QLineEdit, QFrame, QLabel
 from PyQt6.QtGui import QPixmap
@@ -15,17 +14,27 @@ class RegisterAccountUI(QMainWindow):
         self.backButton = self.findChild(QPushButton, "backButton")
         self.registerButton = self.findChild(QPushButton, "registerButton")
         self.addPictureButton = self.findChild(QPushButton, "addPictureButton")
+        self.removePictureButton = self.findChild(QPushButton, "removePictureButton")
         self.inputUserName = self.findChild(QLineEdit, "inputUserName")
         self.inputUserAge = self.findChild(QLineEdit, "inputUserAge")
         self.inputPwd1 = self.findChild(QLineEdit, "inputPwd1")
         self.inputPwd2 = self.findChild(QLineEdit, "inputPwd2")
         self.profilePicture = self.findChild(QFrame, "profilePicture")
+        self.label = QLabel(self.profilePicture)
 
         self.welcomeWindow = None
 
+        self.addPictureClick()
+        self.removePictureClick()
         self.backButtonClick()
         self.registerButtonClick()
         self.loadDefaultImage()
+
+    def addPictureClick(self):
+        pass
+
+    def removePictureClick(self):
+        self.removePictureButton.clicked.connect(self.resetDefaultImage)
 
     def backButtonClick(self):
         self.backButton.clicked.connect(self.openWelcomeUI)
@@ -34,16 +43,26 @@ class RegisterAccountUI(QMainWindow):
         self.registerButton.clicked.connect(self.registerUser)
 
     def loadDefaultImage(self):
-        label = QLabel(self.profilePicture)
         pixmap = QPixmap("../resources/pictures/userDefault.png")
 
-        frame_size = self.profilePicture.size()
-        pixmap = pixmap.scaled(frame_size, QtCore.Qt.AspectRatioMode.KeepAspectRatio,
+        frameSize = self.profilePicture.size()
+        pixmap = pixmap.scaled(frameSize, QtCore.Qt.AspectRatioMode.KeepAspectRatio,
                                QtCore.Qt.TransformationMode.SmoothTransformation)
 
-        label.setPixmap(pixmap)
-        label.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
-        label.setGeometry(self.profilePicture.rect())
+        self.label.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+        self.label.setGeometry(self.profilePicture.rect())
+        self.label.setPixmap(pixmap)
+
+    def resetDefaultImage(self):
+        pixmap = QPixmap("../resources/pictures/userDefault2.png")
+
+        frameSize = self.profilePicture.size()
+        pixmap = pixmap.scaled(frameSize, QtCore.Qt.AspectRatioMode.KeepAspectRatio,
+                               QtCore.Qt.TransformationMode.SmoothTransformation)
+
+        self.label.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+        self.label.setGeometry(self.profilePicture.rect())
+        self.label.setPixmap(pixmap)
 
     def openWelcomeUI(self):
         if not self.welcomeWindow:
