5b5e03d52c54c534baf4e6cf1af602317762ab7f
Pankotai Kristóf Géza <p.kristof1999@gmail.com>
2023-10-01 13:50:54 +0200

    A program betölti listába a bejelentkezési képernyőnél a felhasználóneveket.

--- src/main/python/loginScreen.py
+++ src/main/python/loginScreen.py
@@ -1,7 +1,10 @@
+import os
+import json
+
 import welcomeScreen
 
 from PyQt6.uic import loadUi
-from PyQt6.QtWidgets import QMainWindow, QPushButton
+from PyQt6.QtWidgets import QMainWindow, QPushButton, QLineEdit, QComboBox
 
 from src.main.python.components.securePwd import checkPassword
 
@@ -11,11 +14,30 @@ class LoginScreenUI(QMainWindow):
         super(LoginScreenUI, self).__init__()
         loadUi("../resources/ui/loginScreen.ui", self)
 
+        self.userNameBox = self.findChild(QComboBox, "userNameBox")
+        self.inputPwd = self.findChild(QLineEdit, "inputPwd")
         self.backButton = self.findChild(QPushButton, "backButton")
+        self.loginButton = self.findChild(QPushButton, "loginButton")
 
         self.welcomeWindow = None
 
         self.backButton.clicked.connect(self.openWelcomeUI)
+
+        self.loadUserNames()
+
+    def loadUserNames(self):
+        dataPath = "../../../userdata/profiles/profiles.json"
+
+        try:
+            if os.path.exists(dataPath):
+                with open(dataPath, 'r') as jsonFile:
+                    existingAccounts = json.load(jsonFile)
+
+                    usernames = list(existingAccounts.keys())
+                    self.userNameBox.addItems(usernames)
+
+        except Exception as e:
+            print("Hiba: ", e)
 
     def openWelcomeUI(self):
         if not self.welcomeWindow:
