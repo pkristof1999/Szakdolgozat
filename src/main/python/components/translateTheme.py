d7d3639648d5f037d53cd9589c94c6211060f0e2
Kristóf Géza Pankotai <p.kristof1999@gmail.com>
2024-02-08 11:46:06 +0100

    Magas kontrasztú téma elkészítésének alapozása

--- src/main/python/components/translateTheme.py
+++ src/main/python/components/translateTheme.py
@@ -1,12 +1,14 @@
 def translateTheme(themeName, leftToRight):
     themeMappingLeft = {
         "Alap téma": "default",
-        "Sötét téma": "dark"
+        "Sötét téma": "dark",
+        "Nagy Kontrasztú": "highContrast"
     }
 
     themeMappingRight = {
         "default": "Alap téma",
-        "dark": "Sötét téma"
+        "dark": "Sötét téma",
+        "highContrast": "Nagy Kontrasztú"
     }
 
     return themeMappingLeft.get(themeName, "") if leftToRight else themeMappingRight.get(themeName, "")
