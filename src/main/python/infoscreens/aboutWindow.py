eb6488f921ff78b289b9a45df589827cba8eb91a
Kristóf Géza Pankotai <p.kristof1999@gmail.com>
2024-04-05 17:37:02 +0200

    Névjegyek oldal formázása

--- src/main/python/infoscreens/aboutWindow.py
+++ src/main/python/infoscreens/aboutWindow.py
@@ -2,9 +2,10 @@ import os
 
 from PyQt6.QtGui import QIcon
 from PyQt6.uic import loadUi
-from PyQt6.QtWidgets import QMainWindow, QPushButton
+from PyQt6.QtWidgets import QMainWindow, QPushButton, QLabel, QTextBrowser
 
 from src.main.python.components.logger import logger
+from src.main.python.infoscreens.errorMessage import errorMessage
 
 
 class AboutWindowUI(QMainWindow):
@@ -12,7 +13,6 @@ class AboutWindowUI(QMainWindow):
         super(AboutWindowUI, self).__init__()
 
         self.parent = parent
-        print(parent)
         self.basePath = basePath
 
         self.theme = theme
@@ -21,9 +21,25 @@ class AboutWindowUI(QMainWindow):
 
         self.setFixedSize(self.size())
 
+        self.contentBrowser = self.findChild(QTextBrowser, "contentBrowser")
         self.backButton = self.findChild(QPushButton, "backButton")
 
+        self.loadContent()
+
         self.backButton.clicked.connect(lambda: self.closeWindow(parent))
+
+    def loadContent(self):
+        dataPath = os.path.join(self.basePath, "src/main/resources/datasources/credits_and_sources.txt")
+
+        try:
+            if os.path.exists(dataPath):
+                with open(dataPath, 'r', encoding = "UTF-8") as textFile:
+                    fileContents = textFile.read()
+
+            self.contentBrowser.setText(fileContents)
+
+        except Exception as e:
+            errorMessage(f"Hiba: {e}")
 
     def closeWindow(self, parent):
         logger.info("Visszalépés az üdvözlő képernyőre!")
