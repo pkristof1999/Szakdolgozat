066dc4f1badee851416b58f8239d6c82992edfe3
Kristóf Géza Pankotai <p.kristof1999@gmail.com>
2024-04-16 19:18:10 +0200

    Kvíz ablaknál hiba javítása, kérdések feltöltése

--- src/main/python/quizgame/quizWindow.py
+++ src/main/python/quizgame/quizWindow.py
@@ -191,7 +191,9 @@ class QuizWindowUI(QMainWindow):
                         if button.isChecked():
                             self.arrayOfSolutions.append(button.text())
                             logger.info(f"{button.text()} sikeresen leadva válaszként!")
-                            if button.text() == self.questionBank[self.questionIndex]["rightAnswer"]:
+                            print(button.text())
+                            print(self.questionBank[self.questionIndex - 1]["rightAnswer"])
+                            if button.text() == self.questionBank[self.questionIndex - 1]["rightAnswer"]:
                                 self.goodAnswers += 1
 
                     nextQuestion = self.questionBank[self.questionIndex]
